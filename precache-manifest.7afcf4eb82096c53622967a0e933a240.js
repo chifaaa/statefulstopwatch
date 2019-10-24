3e76de830c770c90de021ed2c920002d3e6ac2af
Murodjon <tmurodon@gmail.com>
2019-10-24 11:27:25 +0100

    Updates

---
+++ precache-manifest.7afcf4eb82096c53622967a0e933a240.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba06882156be78660d1550008c24c86f",
+    "url": "/statefulstopwatch/index.html"
+  },
+  {
+    "revision": "d0eabfbc3e2149741310",
+    "url": "/statefulstopwatch/static/css/main.0bb17c8f.chunk.css"
+  },
+  {
+    "revision": "046fedc5df4ee0930a66",
+    "url": "/statefulstopwatch/static/js/2.22a50106.chunk.js"
+  },
+  {
+    "revision": "d0eabfbc3e2149741310",
+    "url": "/statefulstopwatch/static/js/main.4ed695bb.chunk.js"
+  },
+  {
+    "revision": "8ce1d30537871509d78a",
+    "url": "/statefulstopwatch/static/js/runtime-main.0ee79063.js"
+  }
+]);
